4aa604d2e3cc825fe8cf2d24772a794a03f8bcf7
vnm-nn <69327927+vnm-nn@users.noreply.github.com>
2020-08-10 15:25:12 +0300

    Add files via upload

---
+++ hw_1_1.py
@@ -0,0 +1,13 @@
+# 1. Поработайте с переменными, создайте несколько, выведите на экран,
+# запросите у пользователя несколько чисел и строк и сохраните в переменные,
+# выведите на экран.
+
+student_name = "Leonard"
+student_age = 33
+print("Student's name: ", student_name, "his age ", student_age)
+
+user_name = input("Enter you name: ")
+user_age = int(input("Enter you age: "))
+user_weight = int(input("Enter you weight: "))
+print("Hello", user_name, "did you know, after 10 years you will be: ", user_age + 10)
+
